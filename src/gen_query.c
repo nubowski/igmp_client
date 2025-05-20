bc7feb792a72079a63f515625dfd2aa1ada9bcc9
nubowski <nubowski@nnteam.ru>
2025-05-21 02:04:20 +0300

    fix: invalid args passing for General Query via testing utility

--- src/gen_query.c
+++ src/gen_query.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/ip.h>
@@ -8,53 +9,82 @@
 
 #include "utils.h"
 
-#define DEFAULT_GROUP "224.0.0.1"
+#define DEFAULT_GROUP "0.0.0.0"
 #define INTERFACE "eth0"
 #define QUERY_INTERVAL_SEC 2
 #define REPEAT_COUNT 5
 
 int main(int argc, char *argv[]) {
-    const char *group = DEFAULT_GROUP;
-    if (argc >= 2) {
-        group = argv[1];
+    int group_count = argc > 1 ? argc - 1 : 1;
+    const char **groups = (const char **)malloc(group_count * sizeof(char *));
+    if (!groups) {
+        perror("malloc");
+        return 1;
     }
 
-    printf("[TEST] Sending IGMPv2 Query to %s via %s\n", group, INTERFACE);
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            groups[i - 1] = argv[i];
+        }
+    } else {
+        groups[0] = DEFAULT_GROUP;
+    }
 
     for (int i = 0; i < REPEAT_COUNT; i++) {
-        int sock = socket(AF_INET, SOCK_RAW, IPPROTO_IGMP);
-        if (sock < 0) {
-            perror("socket");
-            return 1;
-        }
+        for (int g = 0; g < group_count; g++) {
+            const char *group = groups[g];
 
-        if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, INTERFACE, strlen(INTERFACE)) < 0) {
-            perror("SO_BINDTODEVICE");
+            struct in_addr group_addr = {0};
+            if (inet_pton(AF_INET, group, &group_addr) != 1) {
+                fprintf(stderr, "[TEST] Invalid IP adress: %s\n", group);
+                continue;
+            }
+
+            // RFC: General Query -> send to 224.0.0.1 with group field = 0.0.0.0
+            int is_generar_query = (strcmp(group, "0.0.0.0") == 0);
+            const char *dst_ip = is_generar_query ? "224.0.0.1" : group;
+
+            printf("[TEST] Sending IGMPv2 %s Query to %s (group field: %s) via %s\n",
+                is_generar_query ? "General" : "Group-Specific", dst_ip, group, INTERFACE);
+
+            int sock = socket(AF_INET, SOCK_RAW, IPPROTO_IGMP);
+            if (sock < 0) {
+                perror("socket");
+                free(groups);
+                return 1;
+            }
+
+            if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, INTERFACE, strlen(INTERFACE)) < 0) {
+                perror("SO_BINDTODEVICE");
+                close(sock);
+                free(groups);
+                return 1;
+            }
+
+            struct igmp msg = {0};
+            msg.igmp_type = 0x11;
+            msg.igmp_code = 20;                                 // Max Resp Time (20 * 100ms = 2s)
+            inet_pton(AF_INET, group, &msg.igmp_group);
+            msg.igmp_cksum = checksum(&msg, sizeof(msg));
+
+            struct sockaddr_in dst = {0};
+            dst.sin_family = AF_INET;
+            inet_pton(AF_INET, dst_ip, &dst.sin_addr);
+
+            ssize_t sent = sendto(sock, &msg, sizeof(msg), 0, (struct sockaddr*)&dst, sizeof(dst));
+            if (sent < 0) {
+                perror("sendto");
+            } else {
+                printf("[TEST] IGMPv2 Query sent to %s (group field: %s)\n", dst_ip, group);
+            }
+
             close(sock);
-            return 1;
         }
 
-        struct igmp msg = {0};
-        msg.igmp_type = 0x11;
-        msg.igmp_code = 20;                                 // Max Resp Time (20 * 100ms = 2s)
-        inet_pton(AF_INET, group, &msg.igmp_group);
-        msg.igmp_cksum = checksum(&msg, sizeof(msg));
-
-        struct sockaddr_in dst = {0};
-        dst.sin_family = AF_INET;
-        inet_pton(AF_INET, group, &dst.sin_addr);
-
-        ssize_t sent = sendto(sock, &msg, sizeof(msg), 0, (struct sockaddr*)&dst, sizeof(dst));
-        if (sent < 0) {
-            perror("sendto");
-        } else {
-            printf("[TEST] IGMPv2 Query sent to %s via %s\n", group, INTERFACE);
-        }
-
-        close(sock);
         sleep(QUERY_INTERVAL_SEC);
     }
 
+    free(groups);
     printf("[TEST] Done\n");
     return 0;
 }
